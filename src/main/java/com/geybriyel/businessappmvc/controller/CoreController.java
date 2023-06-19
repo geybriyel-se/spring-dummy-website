dabd46ad1eb61ff3e29acedc52b1da7f7906ed83
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-19 17:00:36 +0800

    add mapping for home page

--- src/main/java/com/geybriyel/businessappmvc/controller/CoreController.java
+++ src/main/java/com/geybriyel/businessappmvc/controller/CoreController.java
@@ -1,4 +1,12 @@
 package com.geybriyel.businessappmvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CoreController {
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
 }
