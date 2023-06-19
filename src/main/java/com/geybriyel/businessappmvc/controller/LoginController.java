46a0208115036947ce6895a45c0ee52f9b4993ec
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-19 18:45:59 +0800

    add controller for logging in

---
+++ src/main/java/com/geybriyel/businessappmvc/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.geybriyel.businessappmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+}
