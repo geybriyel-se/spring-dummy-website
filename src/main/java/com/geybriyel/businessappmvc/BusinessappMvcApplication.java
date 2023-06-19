0a2c2e7eec7b2fc3b2f246922646d909cc722d39
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-19 16:58:18 +0800

    add core app

---
+++ src/main/java/com/geybriyel/businessappmvc/BusinessappMvcApplication.java
@@ -0,0 +1,13 @@
+package com.geybriyel.businessappmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessappMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessappMvcApplication.class, args);
+	}
+
+}
