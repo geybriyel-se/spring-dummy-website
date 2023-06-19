92986ef1636268246dbb6fa437d6c742624fd55f
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-19 18:42:37 +0800

    embed custom login page to security config

--- src/main/java/com/geybriyel/businessappmvc/security/SecurityConfig.java
+++ src/main/java/com/geybriyel/businessappmvc/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.geybriyel.businessappmvc.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -27,5 +29,18 @@ public class SecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(level1, level2, level3);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer -> configurer
+                        .anyRequest()
+                        .authenticated())
+                .formLogin(form -> form
+                        .loginPage("/loginPage")
+                        .loginProcessingUrl("/authenticateUser")
+                        .permitAll())
+        ;
+        return httpSecurity.build();
     }
 }
