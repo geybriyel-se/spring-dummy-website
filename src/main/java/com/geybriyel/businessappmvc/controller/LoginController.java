73be39e902aba5728a251ea652c49148d7aff026
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-21 17:38:48 +0800

    add request mapping for access denied page

--- src/main/java/com/geybriyel/businessappmvc/controller/LoginController.java
+++ src/main/java/com/geybriyel/businessappmvc/controller/LoginController.java
@@ -2,11 +2,17 @@ package com.geybriyel.businessappmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/loginPage")
     public String showLoginPage() {
         return "login-form";
+    }
+
+    @RequestMapping("accessDenied")
+    public String showAccessDenied() {
+        return "access-denied";
     }
 }
