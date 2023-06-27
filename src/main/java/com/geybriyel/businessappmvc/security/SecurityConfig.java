5aaf9d0fa61bc0995543a4ffd6fdc75b7f526cb0
Gabrielle Sualibio <geybriyelsualibio@gmail.com>
2023-06-27 10:09:24 +0800

    refactor security config to support jdbc user details manager

--- src/main/java/com/geybriyel/businessappmvc/security/SecurityConfig.java
+++ src/main/java/com/geybriyel/businessappmvc/security/SecurityConfig.java
@@ -3,32 +3,18 @@ package com.geybriyel.businessappmvc.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails level1 = User.builder()
-                .username("level1")
-                .password("{noop}password")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails level2 = User.builder()
-                .username("level2")
-                .password("{noop}password")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails level3 = User.builder()
-                .username("level3")
-                .password("{noop}password")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(level1, level2, level3);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
